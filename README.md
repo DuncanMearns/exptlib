898fdcc9ec4e7ca2836a4e6cb77bbcaa77f59ab4
duncan_mearns <mearns@neuro.mpg.de>
2021-11-22 16:18:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Experiment library
+Small library for building experimental analysis pipelines.
+
+Contains basic base classes for building experiment objects, storing metadata, and parallelization of analysis code. 
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="experiment-library",
+    version="0.0.1",
+    author="Duncan Mearns",
+    author_email="mearns@neuro.mpg.de",
+    description="Small library for building experimental analysis pipelines.",
+    python_requires=">=3.7",
+)
