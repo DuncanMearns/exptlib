8ff0cc0045fff92281a210a7c13352474c83e9fe
duncan_mearns <mearns@neuro.mpg.de>
2022-10-05 16:38:17 +0200

    Added directory tree class

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="experiment-library",
-    version="0.0.1",
+    version="0.0.2",
     author="Duncan Mearns",
-    author_email="mearns@neuro.mpg.de",
+    author_email="duncan.mearns@bi.mpg.de",
     description="Small library for building experimental analysis pipelines.",
     packages=setuptools.find_packages(),
-    python_requires=">=3.7"
+    python_requires=">=3.9"
 )
